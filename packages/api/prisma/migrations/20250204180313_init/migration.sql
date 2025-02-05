26e16de1d0e040839ff52fd5cf360039baf6d4d9
Tanmay Mazumdar <mazumdar.t19@gmail.com>
2025-02-05 19:22:56 +0530

    feat(DAZ-12): prisma schema changes

--- packages/api/prisma/migrations/20250204180313_init/migration.sql
+++ packages/api/prisma/migrations/20250204180313_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "users" (
     "id" VARCHAR(8) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -10,7 +11,8 @@ CREATE TABLE "users" (
 CREATE TABLE "profile" (
     "id" VARCHAR(12) NOT NULL,
     "name" VARCHAR(255) NOT NULL,
-    "userId" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "createad_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "profile_pkey" PRIMARY KEY ("id")
 );
@@ -19,7 +21,8 @@ CREATE TABLE "profile" (
 CREATE TABLE "cereal" (
     "id" VARCHAR(12) NOT NULL,
     "key" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "createad_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "cereal_pkey" PRIMARY KEY ("id")
 );
@@ -34,17 +37,16 @@ CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 CREATE UNIQUE INDEX "profile_id_key" ON "profile"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "profile_userId_key" ON "profile"("userId");
+CREATE UNIQUE INDEX "profile_user_id_key" ON "profile"("user_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "cereal_id_key" ON "cereal"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "cereal_userId_key" ON "cereal"("userId");
+CREATE UNIQUE INDEX "cereal_user_id_key" ON "cereal"("user_id");
 
 -- AddForeignKey
-ALTER TABLE "profile" ADD CONSTRAINT "profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "profile" ADD CONSTRAINT "profile_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "cereal" ADD CONSTRAINT "cereal_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-
+ALTER TABLE "cereal" ADD CONSTRAINT "cereal_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
